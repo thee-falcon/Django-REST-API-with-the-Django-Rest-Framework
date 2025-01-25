164bac67136e69783a44fad79b4a71a0e6fdf3e3
thee-falcon <omar.makran.10@gmail.com>
2025-01-25 19:36:15 +0100

    get objects from product app to creaate an api for it

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+from product.models import Product
 
 # Create your views here.
 def api_home(request, *args, **kwargs):
-    
-    # print(request.GET) # url query params
-    
-    body = request.body # byte string of json data
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body) # convert the byte string to json string
-    except:
-        pass
-    data["params"] = dict(request.GET)
-    data["headers"] = dict(request.headers)
-    data["content_type"] = request.content_type
+    
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
