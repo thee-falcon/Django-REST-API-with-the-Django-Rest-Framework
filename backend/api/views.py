0200dda04e52ae41edc2b71885fbc115a3af2d7e
thee-falcon <omar.makran.10@gmail.com>
2025-01-25 18:01:36 +0100

    creat the view for the api

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 
 # Create your views here.
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hello there, this is your django API response!"})
+    
+    # print(request.GET) # url query params
+    
+    body = request.body # byte string of json data
+    data = {}
+    try:
+        data = json.loads(body) # convert the byte string to json string
+    except:
+        pass
+    data["params"] = dict(request.GET)
+    data["headers"] = dict(request.headers)
+    data["content_type"] = request.content_type
+    return JsonResponse(data)
