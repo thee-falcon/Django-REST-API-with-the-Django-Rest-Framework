0946740b7b2c55cdf96408ae83bc91a6615d0297
thee-falcon <omar.makran.10@gmail.com>
2025-01-25 20:05:40 +0100

    model to dictionary

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import json
 from product.models import Product
+from django.forms.models import model_to_dict
 
 # Create your views here.
 def api_home(request, *args, **kwargs):
@@ -9,8 +10,5 @@ def api_home(request, *args, **kwargs):
     data = {}
     
     if model_data:
-        data['id'] = model_data.id
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
     return JsonResponse(data)
