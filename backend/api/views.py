ddb0fd0334e03d423540a479b56b82b0435ce1ca
thee-falcon <omar.makran.10@gmail.com>
2025-01-26 23:44:25 +0100

    handle the POST request

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,12 +4,21 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from product.serializers import ProductSrializer
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def api_home(request, *args, **kwargs):
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
     
-    if instance:
-        # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price', ''])
-        data = ProductSrializer(instance).data
-    return Response(data)
+    if request.method == 'GET':
+        instance = Product.objects.all().first()
+        data = {}
+
+        if instance:
+            # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price', ''])
+            data = ProductSrializer(instance).data
+        return Response(data)
+    elif request.method == 'POST':
+        serializer = ProductSrializer(data=request.data) # if we want let a client know about the issue.
+        if serializer.is_valid(raise_exception=True):
+            instance = serializer.save()
+            print(serializer.data)
+            return Response(serializer.data)
+        return Response({"invalid": "Not good data"}, status=400) # if we don't want a client know about the issue.
