31a68deb18a0e1ab6fbcb08b1c3b14631b1cb6b6
thee-falcon <omar.makran.10@gmail.com>
2025-01-26 23:47:01 +0100

    try to post a request to the API

--- py_client/basic.py
+++ py_client/basic.py
@@ -5,7 +5,10 @@ import requests
 endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World!"})
+print(get_response.json())
+
+post_response = requests.post(endpoint, params={"abc": 123}, json={"title": None})
+print(post_response.json())
 
 # print (get_response.text)
-print(get_response.json())
 # print(get_response.status_code)
