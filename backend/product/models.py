d1d588b0e86517912b90c4332518f2755672654f
thee-falcon <omar.makran.10@gmail.com>
2025-01-26 20:11:10 +0100

    add sale_price using property decorator and get_discount to data

--- backend/product/models.py
+++ backend/product/models.py
@@ -6,3 +6,9 @@ class Product(models.Model):
     content = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=15, decimal_places=2, default=0.0)
     
+    @property
+    def sale_price(self):
+        return "%.2f" %(float(self.price) * 0.8)
+    
+    def get_dicount(self):
+        return "200"
