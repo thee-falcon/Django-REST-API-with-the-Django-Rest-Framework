00ddb5a5ee90d60da182e4a55c4965d481038a66
thee-falcon <omar.makran.10@gmail.com>
2025-01-28 19:39:18 +0100

    creating the api view  and perform create (handling data) for the server part

--- backend/product/views.py
+++ backend/product/views.py
@@ -6,3 +6,16 @@ from .serializers import ProductSrializer
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all() # and we can change queryset using function: def get_queryset()
     serializer_class = ProductSrializer
+    
+class ProductCreatAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSrializer
+    
+    # handle the json data from the client(POST request)
+    def perform_create(self, serializer):
+        # print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
