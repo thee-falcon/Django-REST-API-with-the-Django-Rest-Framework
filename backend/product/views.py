c38abc0f31c63039fe831fc2943c2856c17bad32
thee-falcon <omar.makran.10@gmail.com>
2025-01-28 18:06:16 +0100

    working with genericss from rest_framework

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Product
+from rest_framework import generics
+from .serializers import ProductSrializer
 
-# Create your views here.
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all() # and we can change queryset using function: def get_queryset()
+    serializer_class = ProductSrializer
