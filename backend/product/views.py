989ace07ee557864d72f1af43954a035ce19bb49
thee-falcon <omar.makran.10@gmail.com>
2025-01-28 23:56:34 +0100

    working wirh ListCreatAPIView and create a function to hadnle curd requests

--- backend/product/views.py
+++ backend/product/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from .models import Product
 from rest_framework import generics
 from .serializers import ProductSrializer
+from rest_framework.decorators import api_view
+from django.shortcuts import get_object_or_404
+from requests import Response
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all() # and we can change queryset using function: def get_queryset()
     serializer_class = ProductSrializer
     
-class ProductCreatAPIView(generics.CreateAPIView):
+class ProductListCreatAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSrializer
     
@@ -19,3 +22,19 @@ class ProductCreatAPIView(generics.CreateAPIView):
         if content is None:
             content = title
         serializer.save(content=content)
+ 
+
+@api_view(['GET', 'POST'])    
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+    
+    if method == 'GET':
+        # detail view
+        if pk is not None:
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSrializer(obj, Many=False).data
+            return Response(data)
+        # list view
+        queryset = Product.objects.all()
+        data = ProductSrializer(queryset, Many=True).data
+        return Response(data)
