e0bc094d7db90ff19d9c32a6f2f34147b3c818c9
thee-falcon <omar.makran.10@gmail.com>
2025-01-29 18:29:36 +0100

    handle GET and POST in one single function

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('<int:pk>/', views.ProductDetailAPIView.as_view()),
     path('',  views.ProductListCreatAPIView.as_view()),
+    # path('<int:pk>/', views.product_alt_view),
+    # path('',  views.product_alt_view),
 ]
--- backend/product/views.py
+++ backend/product/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics
 from .serializers import ProductSrializer
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
-from requests import Response
+from rest_framework.response import Response
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all() # and we can change queryset using function: def get_queryset()
@@ -24,6 +24,7 @@ class ProductListCreatAPIView(generics.ListCreateAPIView):
         serializer.save(content=content)
  
 
+# another way to handle crud request in one single function
 @api_view(['GET', 'POST'])    
 def product_alt_view(request, pk=None, *args, **kwargs):
     method = request.method
@@ -32,9 +33,20 @@ def product_alt_view(request, pk=None, *args, **kwargs):
         # detail view
         if pk is not None:
             obj = get_object_or_404(Product, pk=pk)
-            data = ProductSrializer(obj, Many=False).data
+            data = ProductSrializer(obj, many=False).data
             return Response(data)
         # list view
         queryset = Product.objects.all()
-        data = ProductSrializer(queryset, Many=True).data
+        data = ProductSrializer(queryset, many=True).data
         return Response(data)
+    elif method == 'POST':
+        # create the item to store the data from the request
+        serializer = ProductSrializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+                serializer.save(content=content)
+        return Response(serializer.data)
+    return Response({"invalid": "not good data"}, status=400)
