89a75e634eff84d9483f31bfab3cca1f3bb2338f
thee-falcon <omar.makran.10@gmail.com>
2025-01-31 21:31:04 +0100

    add the url for update products

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('<int:pk>/', views.ProductDetailAPIView.as_view()),
     path('',  views.ProductListCreatAPIView.as_view()),
+    path('update/<int:pk>/', views.ProductUpdateCreateView.as_view()),
     # path('<int:pk>/', views.product_alt_view),
     # path('',  views.product_alt_view),
 ]
